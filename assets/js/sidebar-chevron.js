daace9ff7ced9ee7558a32208524850c35d91b64
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-04-19 12:19:21 +0200

    feat: sidebar style

--- assets/js/sidebar-chevron.js
+++ assets/js/sidebar-chevron.js
@@ -1,8 +1,15 @@
-const sidebar = document.querySelector('#sidebar__sms')
+const sidebars = document.querySelectorAll('.sidebar')
 
+sidebars.forEach((sidebar) => {
+    const links = sidebar.querySelectorAll('[data-bs-toggle="collapse"]')
 
-let rotate = +sidebar.querySelector('.bi-chevron-down').style.rotate
-sidebar.addEventListener('click', () => {
-    rotate += 180
-    sidebar.querySelector('.bi-chevron-down').style.rotate = rotate + "deg";
+    links.forEach((e) => {
+        let rotate = +e.querySelector('.bi-chevron-down').style.rotate
+        e.addEventListener('click', () => {
+            rotate += 180
+            e.querySelector('.bi-chevron-down').style.rotate = rotate + "deg";
+        })
+    })
 })
+
+
