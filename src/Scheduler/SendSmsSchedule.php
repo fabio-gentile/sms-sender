a50193decb78b4351f0a5073fe2c9c6d9d5bcb7b
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-05-07 15:11:06 +0200

    feat: scheduler for planified sms

---
+++ src/Scheduler/SendSmsSchedule.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Scheduler;
+
+use App\Message\SendSms;
+use Symfony\Component\Scheduler\Attribute\AsSchedule;
+use Symfony\Component\Scheduler\RecurringMessage;
+use Symfony\Component\Scheduler\Schedule;
+use Symfony\Component\Scheduler\ScheduleProviderInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+
+#[AsSchedule('sms')]
+final class SendSmsSchedule implements ScheduleProviderInterface
+{
+    public function __construct(
+        private CacheInterface $cache,
+    ) {
+    }
+
+    public function getSchedule(): Schedule
+    {
+        return (new Schedule())
+            ->add(
+                RecurringMessage::every('1 minute', new SendSms(rand(1, 999999))),
+            )
+            ->stateful($this->cache)
+        ;
+    }
+}
